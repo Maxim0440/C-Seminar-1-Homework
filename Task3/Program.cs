14ca79b71af9ee3a2ec056ba2112ca9395fae140
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-03 13:34:41 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int reminder = number % 2;
+
+if (reminder > 0)
+{
+    System.Console.WriteLine("нечетное");
+}
+else 
+{
+System.Console.WriteLine("четное");
+}
